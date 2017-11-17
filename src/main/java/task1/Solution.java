0476a635556c43027e8885b12e1e233ddc70cb34
Igor <igor_vasilev@epam.com>
2017-11-17 14:51:17 +0300

    first try

--- src/main/java/task1/Solution.java
+++ src/main/java/task1/Solution.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Solution {
   private static boolean[][] visited;
+  private static int n;
+  private static int m;
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
-    int m = in.nextInt();
+    n = in.nextInt();
+    m = in.nextInt();
     int grid[][] = new int[n][m];
     for(int grid_i=0; grid_i < n; grid_i++){
       for(int grid_j=0; grid_j < m; grid_j++){
@@ -39,17 +41,27 @@ public class Solution {
   }
 
   private static int dfs(int row, int col) {
-    int count = 0;
-    for (int i = -1; i < 1; i++) {
-      for (int j = -1; j < 1; j++) {
+    int count = 1;
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
         int curRow = row + i;
         int curCol = col + j;
-        if (curRow > 0 && curCol > 0 && !visited[curRow][curCol]) {
-
+        if (isValid(curRow, curCol)) {
+          visited[curRow][curCol] = true;
+          count++;
+          count += dfs(curRow, curCol);
+        } else {
+          return 0;
         }
       }
 
     }
+
+    return count;
+  }
+
+  private static boolean isValid(int curRow, int curCol) {
+    return curRow >= 0 && curCol >= 0 && curRow < n && curCol < m && !visited[curRow][curCol];
   }
 
   private static boolean inRange(Integer n) {
