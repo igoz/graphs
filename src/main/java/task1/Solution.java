d8462e4ace66f52e2e90e6e1ca21c765deba075a
Igor <igor_vasilev@epam.com>
2017-11-17 14:59:47 +0300

    final solution

--- src/main/java/task1/Solution.java
+++ src/main/java/task1/Solution.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Solution {
   private static boolean[][] visited;
+  private static int[][] grid;
   private static int n;
   private static int m;
 
@@ -11,7 +12,7 @@ public class Solution {
     Scanner in = new Scanner(System.in);
     n = in.nextInt();
     m = in.nextInt();
-    int grid[][] = new int[n][m];
+    grid = new int[n][m];
     for(int grid_i=0; grid_i < n; grid_i++){
       for(int grid_j=0; grid_j < m; grid_j++){
         grid[grid_i][grid_j] = in.nextInt();
@@ -27,11 +28,9 @@ public class Solution {
     int max = 0;
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
-        if (grid[i][j] == 1 && !visited[i][j]) {
           int count = dfs(i, j);
           if (max < count) {
             max = count;
-          }
         }
       }
     }
@@ -41,18 +40,17 @@ public class Solution {
   }
 
   private static int dfs(int row, int col) {
+    if (!isValid(row, col)) {
+      return 0;
+    }
+
+    visited[row][col] = true;
     int count = 1;
     for (int i = -1; i <= 1; i++) {
       for (int j = -1; j <= 1; j++) {
         int curRow = row + i;
         int curCol = col + j;
-        if (isValid(curRow, curCol)) {
-          visited[curRow][curCol] = true;
-          count++;
-          count += dfs(curRow, curCol);
-        } else {
-          return 0;
-        }
+        count += dfs(curRow, curCol);
       }
 
     }
@@ -61,7 +59,7 @@ public class Solution {
   }
 
   private static boolean isValid(int curRow, int curCol) {
-    return curRow >= 0 && curCol >= 0 && curRow < n && curCol < m && !visited[curRow][curCol];
+    return curRow >= 0 && curCol >= 0 && curRow < n && curCol < m && grid[curRow][curCol] == 1 && !visited[curRow][curCol];
   }
 
   private static boolean inRange(Integer n) {
